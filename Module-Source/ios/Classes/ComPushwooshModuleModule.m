2ac8d18ca7c246c7b1661dc8c615ffa43f799864
diman <diman@pushwoosh.com>
2016-03-21 19:02:27 +0600

    Refactor and use ti-module best practises

--- Module-Source/ios/Classes/ComPushwooshModuleModule.m
+++ Module-Source/ios/Classes/ComPushwooshModuleModule.m
@@ -10,23 +10,7 @@
 #import "TiHost.h"
 #import "TiUtils.h"
 
-#import <Pushwoosh/PushNotificationManager.h>
-
 static __strong NSDictionary * gStartPushData = nil;
-
-static id objectOrNull(id object)
-{
-	return object ?: [NSNull null];
-}
-
-static BOOL checkArgument(id argument, Class expectedClass)
-{
-	if (!argument || ! [argument isKindOfClass:expectedClass]) {
-		NSLog(@"[ERROR][PW-APPC]: Invalid argument. Expected argument of type %@", expectedClass);
-		return NO;
-	}
-	return YES;
-}
 
 @implementation ComPushwooshModuleModule
 
@@ -52,96 +36,56 @@ static BOOL checkArgument(id argument, Class expectedClass)
 	// you *must* call the superclass
 	[super startup];
 	
-	self.registered = NO;
+	[self setRegistered:NO];
 	
 	PushNotificationManager * pushManager = [PushNotificationManager pushManager];
-	pushManager.showPushnotificationAlert = NO;
-	pushManager.delegate = self;
+	[pushManager setShowPushnotificationAlert:NO];
+	[pushManager setDelegate:self];
 
 	NSLog(@"[INFO][PW-APPC] %@ loaded", self);
-}
-
-- (void)shutdown:(id)sender
-{
-	// this method is called when the module is being unloaded
-	// typically this is during shutdown. make sure you don't do too
-	// much processing here or the app will be quit forceably
-
-	// you *must* call the superclass
-	[super shutdown:sender];
-}
-
-#pragma mark Internal Memory Management
-
-- (void)didReceiveMemoryWarning:(NSNotification*)notification
-{
-	// optionally release any resources that can be dynamically
-	// reloaded once memory is available - such as caches
-	[super didReceiveMemoryWarning:notification];
-}
-
-#pragma mark Listener Notifications
-
-- (void)_listenerAdded:(NSString *)type count:(int)count
-{
-	if (count == 1 && [type isEqualToString:@"my_event"])
-	{
-		// the first (of potentially many) listener is being added
-		// for event named 'my_event'
-	}
-}
-
-- (void)_listenerRemoved:(NSString *)type count:(int)count
-{
-	if (count == 0 && [type isEqualToString:@"my_event"])
-	{
-		// the last listener called for event named 'my_event' has
-		// been removed, we can optionally clean up any resources
-		// since no body is listening at this point for that event
-	}
 }
 
 #pragma Public APIs
 
 - (void)pushNotificationsRegister:(id)args
 {
-	NSArray *argumentsArray = args;
-	if(!checkArgument(argumentsArray, [NSArray class]))
-		return;
+	ENSURE_TYPE(args, NSArray);
+	ENSURE_ARG_COUNT(args, 1);
 	
-	if([argumentsArray count] != 1)
-	{
-		NSLog(@"[ERROR][PW-APPC] pushNotificationsRegister, expected 1 argument");
-		return;
+	ENSURE_TYPE(args[0], NSDictionary)
+	NSDictionary *options = args[0];
+	
+	if (options[@"success"]) {
+		ENSURE_TYPE(options[@"success"], KrollCallback);
 	}
 	
-	NSDictionary *options = [argumentsArray objectAtIndex:0];
-	if(!checkArgument(options, [NSDictionary class]))
-	   return;
+	if (options[@"error"]) {
+		ENSURE_TYPE(options[@"error"], KrollCallback);
+	}
 	
-	self.successCallback = [options objectForKey:@"success"];
-	self.errorCallback = [options objectForKey:@"error"];
-	self.messageCallback = [options objectForKey:@"callback"];
+	ENSURE_TYPE(options[@"callback"], KrollCallback);
 	
-	if(!checkArgument(self.messageCallback, [KrollCallback class]))
-		return;
-	
-	NSString* appCode = [options objectForKey:@"pw_appid"];
-	if(!checkArgument(appCode, [NSString class]))
-		return;
+	self.successCallback = options[@"success"];
+	self.errorCallback = options[@"error"];
+	self.messageCallback = options[@"callback"];
+		
+	NSString* appCode = options[@"pw_appid"];
+	ENSURE_TYPE(appCode, NSString);
 	
 	PushNotificationManager * pushManager = [PushNotificationManager pushManager];
 	[[NSUserDefaults standardUserDefaults] setObject:appCode forKey:@"Pushwoosh_APPID"];
 	//we need to re-set APPID if it has been changed (on start we have initialized Push Manager with app id from NSUserDefaults)
-	pushManager.appCode = appCode;
+	[pushManager setAppCode:appCode];
 
 	// register for push notifications!
+	NSLog(@"[DEBUG][PW-APPC] registering for push notifications");
 	[[PushNotificationManager pushManager] registerForPushNotifications];
  
-	if (gStartPushData && !self.registered)
+	if (gStartPushData && !self.registered) {
 		[self performSelectorOnMainThread:@selector(dispatchPush:) withObject:gStartPushData waitUntilDone:YES];
+	}
 		 
-	self.registered = YES;
+	[self setRegistered:YES];
 }
 
 - (void)unregister:(id)unused
@@ -162,46 +106,41 @@ static BOOL checkArgument(id argument, Class expectedClass)
 - (void)setTags:(id)args
 {
 	NSDictionary *tags = args;
-	if ([args isKindOfClass: [NSArray class]])
-	{
+	if ([args isKindOfClass: [NSArray class]]) {
 		// some versions of Titanium may pass argument in array
-		tags = [((NSArray*)args) objectAtIndex: 0];
+		tags = args[0];
 	}
 	
-	checkArgument(tags, [NSDictionary class]);
+	ENSURE_TYPE(tags, NSDictionary);
 	
 	[[PushNotificationManager pushManager] setTags:tags];
 }
 
 - (void)getLaunchNotification:(id)args
 {
-	NSArray *argumentsArray = args;
-	NSDictionary *options = [argumentsArray objectAtIndex:0];
-	checkArgument(options, [NSDictionary class]);
+	ENSURE_TYPE(args, NSArray);
+	ENSURE_TYPE(args[0], NSDictionary);
 	
-	KrollCallback *callback = [options objectForKey:@"callback"];
-	checkArgument(callback, [KrollCallback class]);
+	NSDictionary *options = args[0];
+	KrollCallback *callback = options[@"callback"];
 	
-	dispatch_async(dispatch_get_main_queue(), ^{
-		[callback call:[NSArray arrayWithObject:objectOrNull(gStartPushData)] thisObject:nil];
-	});
+	ENSURE_TYPE(callback, KrollCallback);
 	
+	TiThreadPerformOnMainThread(^{
+		[callback call:@[ gStartPushData ?: [NSNull null] ] thisObject:nil];
+	}, NO);
 }
 
 - (void)onDidRegisterForRemoteNotificationsWithDeviceToken:(NSString *)token
 {
-	NSLog(@"[INFO][PW-APPC] registered for pushes: %@", token);
-
-	NSDictionary *result = [NSDictionary dictionaryWithObject:token forKey:@"registrationId"];
-	[self.successCallback call:[NSArray arrayWithObject:result] thisObject:nil];
+	NSLog(@"[DEBUG][PW-APPC] registered for pushes: %@", token);
+	[self.successCallback call:@[ @{ @"registrationId" : token} ] thisObject:nil];
 }
 
 - (void)onDidFailToRegisterForRemoteNotificationsWithError:(NSError*)error
 {
 	NSLog(@"[DEBUG][PW-APPC] failed to register for pushes: %@", error.localizedDescription);
-
-	NSDictionary *result = [NSDictionary dictionaryWithObject:error.localizedDescription forKey:@"error"];
-	[self.errorCallback call:[NSArray arrayWithObject:result] thisObject:nil];
+	[self.errorCallback call:@[ @{ @"error" : error.localizedDescription} ] thisObject:nil];
 }
 
 - (void)onPushAccepted:(PushNotificationManager *)manager withNotification:(NSDictionary *)pushNotification onStart:(BOOL)onStart
@@ -211,8 +150,9 @@ static BOOL checkArgument(id argument, Class expectedClass)
 	//reset badge counter
 	[[UIApplication sharedApplication] setApplicationIconBadgeNumber:0];
 
-	if (onStart)
+	if (onStart) {
 		gStartPushData = pushNotification;
+	}
 	
 	[self dispatchPush:pushNotification];
 }
@@ -220,9 +160,7 @@ static BOOL checkArgument(id argument, Class expectedClass)
 - (void) dispatchPush:(NSDictionary*)pushData
 {
 	NSLog(@"[INFO][PW-APPC] dispatch push: %@", pushData);
-
-	NSDictionary *result = [NSDictionary dictionaryWithObject:pushData forKey:@"data"];
-	[self.messageCallback call:[NSArray arrayWithObject:result] thisObject:nil];
+	[self.messageCallback call:@[ @{ @"data" : pushData } ] thisObject:nil];
 }
 
 @end
@@ -238,8 +176,9 @@ static BOOL checkArgument(id argument, Class expectedClass)
 {
 	NSLog(@"[DEBUG][PW-APPC] UIApplication(InternalPushRuntime) push accepted: onStart: %d, payload: %@", onStart, pushNotification);
 
-	if (onStart)
+	if (onStart) {
 		gStartPushData = pushNotification;
+	}
 }
 
 // Set delegate to self on start as the module has not been created yet.
