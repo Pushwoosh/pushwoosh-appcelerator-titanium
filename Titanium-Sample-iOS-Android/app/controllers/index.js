f5705fd83741db8da27460caeceaa7bc2081af17
Beau Rushton <beaurushton@gmail.com>
2016-05-16 10:10:08 +1000

    Updated sample code

--- Titanium-Sample-iOS-Android/app/controllers/index.js
+++ Titanium-Sample-iOS-Android/app/controllers/index.js
@@ -9,19 +9,25 @@ Ti.API.info("module is => " + pushnotifications);
 
 pushnotifications.pushNotificationsRegister({
   "pw_appid": "4FC89B6D14A655.46488481",
-  "gcm_projectid": "60756016005",
+  "gcm_projectid": "60756016005", // please note this is the project "number" not the "id" when viewed in the google API console.  You can find this under the project settings.
   success:function(e)
   {
+    setTimeout(function() {
       Ti.API.info('JS registration success event: ' + e.registrationId);
+    }, 0);
   },
   error:function(e)
   {
+    setTimeout(function() {
       Ti.API.error("Error during registration: "+e.error);
+    }, 0);
   },
   callback:function(e) // called when a push notification is received
   {
+    setTimeout(function() {
+      //push notifications title: e.data.aps.alert
       Ti.API.info('JS message event: ' + JSON.stringify(e.data));
-      alert('JS message event: ' + JSON.stringify(e.data));
+    }, 0);
   }
 });
 
@@ -32,4 +38,3 @@ pushnotifications.setTags({"MyTag":["hello", "world"]});
 
 pushnotifications.startTrackingGeoPushes();
 pushnotifications.stopTrackingGeoPushes();
-
