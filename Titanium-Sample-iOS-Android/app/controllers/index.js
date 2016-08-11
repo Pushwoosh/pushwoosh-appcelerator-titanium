0929ba10c362ac9f049428b69dfa212183165f0d
diman <diman@pushwoosh.com>
2016-08-11 11:21:17 +0600

    Sample UI

--- Titanium-Sample-iOS-Android/app/controllers/index.js
+++ Titanium-Sample-iOS-Android/app/controllers/index.js
@@ -1,6 +1,3 @@
-function doClick(e) {
-    alert($.label.text);
-}
 
 $.index.open();
 
@@ -13,20 +10,25 @@ pushwoosh.onPushReceived(function(e) {
 
 pushwoosh.onPushOpened(function(e) {
 	Ti.API.info('Push notification opened: ' + JSON.stringify(e));
+	$.pushMessage.text = e.message;
+	$.pushData.text = JSON.stringify(e);
 });
 
 pushwoosh.initialize({ 
     "application" : "4FC89B6D14A655.46488481",
-    "gcm_project" : "60756016005",
+    "gcm_project" : "60756016005"
 });
 
 pushwoosh.registerForPushNotifications(
 	function(e) {
         Ti.API.info('JS registration success event: ' + e.registrationId);
         Ti.API.info('Push token ' + pushwoosh.getPushToken());
+        $.pushRegistration.text = "Registered with token: " + e.registrationId;
+        
     },
     function(e) {
         Ti.API.error("Error during registration: " + e.error);
+        $.pushRegistration.text = "Failed to register: " + e.error;
     }  
 );
 
