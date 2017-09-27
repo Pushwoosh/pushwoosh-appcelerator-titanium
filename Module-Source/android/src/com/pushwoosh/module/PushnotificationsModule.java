3a8b45a85e184042c45e3a858c404ceb06b626c9
Mikhail Savin <smu@arello-mobile.com>
2017-09-27 15:46:23 +0700

    [artifacts][Android] Code refactoring

--- Module-Source/android/src/com/pushwoosh/module/PushnotificationsModule.java
+++ Module-Source/android/src/com/pushwoosh/module/PushnotificationsModule.java
@@ -1,7 +1,6 @@
 /**
  * Pushwoosh SDK
  * (c) Pushwoosh 2012
- *
  */
 package com.pushwoosh.module;
 
@@ -46,9 +45,8 @@ import org.json.JSONObject;
 
 import static android.R.attr.data;
 
-@Kroll.module(name="Pushwoosh", id="com.pushwoosh.module")
-public class PushnotificationsModule extends KrollModule
-{
+@Kroll.module(name = "Pushwoosh", id = "com.pushwoosh.module")
+public class PushnotificationsModule extends KrollModule {
 	public static PushnotificationsModule INSTANCE = null;
 
 	private static final String LCAT = "PushnotificationsModule";
@@ -69,14 +67,12 @@ public class PushnotificationsModule extends KrollModule
 
 	// Mandatory method
 	@Kroll.onAppCreate
-	public static void onAppCreate(TiApplication app)
-	{
+	public static void onAppCreate(TiApplication app) {
 		Log.d(LCAT, "inside onAppCreate");
 		// put module init code that needs to run when the application is created
 	}
 
-	public PushnotificationsModule()
-	{
+	public PushnotificationsModule() {
 		super();
 		INSTANCE = this;
 		Log.d(LCAT, "Push: create module");
@@ -92,20 +88,19 @@ public class PushnotificationsModule extends KrollModule
 				broadcastPush = ai.metaData.getBoolean("PW_BROADCAST_PUSH", false);
 			}
 
-		} catch(Exception e) {
+		} catch (Exception e) {
 			Log.e(LCAT, "Failed to read AndroidManifest metaData", e);
 		}
 	}
 
 	@Kroll.method
-	public void initialize(HashMap options)
-	{
+	public void initialize(HashMap options) {
 		Log.d(LCAT, "initialize called");
 
 		initialized.set(true);
 
-		String pushwooshAppId = (String)options.get("application");
-		String googleProjectId = (String)options.get("gcm_project");
+		String pushwooshAppId = (String) options.get("application");
+		String googleProjectId = (String) options.get("gcm_project");
 
 		// dispatch saved start notification
 		String startPush = startPushData.getAndSet(null);
@@ -119,27 +114,24 @@ public class PushnotificationsModule extends KrollModule
 	}
 
 	@Kroll.method
-	public void onPushOpened(KrollFunction callback)
-	{
+	public void onPushOpened(KrollFunction callback) {
 		pushOpenCallback = callback;
 	}
 
 	@Kroll.method
-	public void onPushReceived(KrollFunction callback)
-	{
+	public void onPushReceived(KrollFunction callback) {
 		pushReceiveCallback = callback;
 	}
 
 	@Deprecated
 	@Kroll.method
-	public void pushNotificationsRegister(HashMap options)
-	{
+	public void pushNotificationsRegister(HashMap options) {
 		Log.w(LCAT, "<pushNotificationsRegister> method is deprecated! Use <initialize> and <register> instead");
 
 		KrollFunction success = (KrollFunction) options.get("success");
 		KrollFunction error = (KrollFunction) options.get("error");
 
-		messageCallback = (KrollFunction)options.get("callback");
+		messageCallback = (KrollFunction) options.get("callback");
 
 		initialize(options);
 
@@ -149,8 +141,7 @@ public class PushnotificationsModule extends KrollModule
 	}
 
 	@Kroll.method
-	public void registerForPushNotifications(KrollFunction success, KrollFunction error)
-	{
+	public void registerForPushNotifications(KrollFunction success, KrollFunction error) {
 		registrationSuccessCallback.set(success);
 		registrationErrorCallback.set(error);
 
@@ -188,14 +179,12 @@ public class PushnotificationsModule extends KrollModule
 	}
 
 	@Kroll.method
-	public void setTags(HashMap params)
-	{
+	public void setTags(HashMap params) {
 		Pushwoosh.getInstance().sendTags(Tags.fromJson(JsonUtils.mapToJson(params)), null);
 	}
 
 	@Kroll.method
-	public void getTags(final KrollFunction success, final KrollFunction error)
-	{
+	public void getTags(final KrollFunction success, final KrollFunction error) {
 		Pushwoosh.getInstance().getTags(new Callback<TagsBundle, GetTagsException>() {
 			@Override
 			public void process(Result<TagsBundle, GetTagsException> result) {
@@ -215,8 +204,7 @@ public class PushnotificationsModule extends KrollModule
 	}
 
 	@Kroll.method
-	public int scheduleLocalNotification(String message, int seconds)
-	{
+	public int scheduleLocalNotification(String message, int seconds) {
 		LocalNotification notification = new LocalNotification.Builder()
 				.setMessage(message)
 				.setDelay(seconds)
@@ -226,74 +214,62 @@ public class PushnotificationsModule extends KrollModule
 	}
 
 	@Kroll.method
-	public void clearLocalNotification(int id)
-	{
+	public void clearLocalNotification(int id) {
 		LocalNotificationReceiver.cancelNotification(id);
 	}
 
 	@Kroll.method
-	public void clearLocalNotifications()
-	{
+	public void clearLocalNotifications() {
 		LocalNotificationReceiver.cancelAll();
 	}
 
 	@Kroll.method
-	public void setMultiNotificationMode()
-	{
+	public void setMultiNotificationMode() {
 		PushwooshNotificationSettings.setMultiNotificationMode(true);
 	}
 
 	@Kroll.method
-	public void setSimpleNotificationMode()
-	{
+	public void setSimpleNotificationMode() {
 		PushwooshNotificationSettings.setMultiNotificationMode(false);
 	}
 
 	@Kroll.method
-	public void setBadgeNumber(int badgeNumber)
-	{
+	public void setBadgeNumber(int badgeNumber) {
 		PushwooshBadge.setBadgeNumber(badgeNumber);
 	}
 
 	@Kroll.method
-	public int getBadgeNumber()
-	{
+	public int getBadgeNumber() {
 		return PushwooshBadge.getBadgeNumber();
 	}
 
 	@Kroll.method
-	public void addBadgeNumber(int deltaBadge)
-	{
+	public void addBadgeNumber(int deltaBadge) {
 		PushwooshBadge.addBadgeNumber(deltaBadge);
 	}
 
 	@Kroll.method
-	public void setUserId(String userId)
-	{
+	public void setUserId(String userId) {
 		PushwooshInApp.getInstance().setUserId(userId);
 	}
 
 	@Kroll.method
-	public void postEvent(String event, HashMap attributes)
-	{
-		PushwooshInApp.getInstance().postEvent(event, Tags.fromJson(JsonUtils.mapToJson((Map<String, Object>)attributes)));
+	public void postEvent(String event, HashMap attributes) {
+		PushwooshInApp.getInstance().postEvent(event, Tags.fromJson(JsonUtils.mapToJson((Map<String, Object>) attributes)));
 	}
 
 	@Kroll.method
-	public String getHwid()
-	{
+	public String getHwid() {
 		return Pushwoosh.getInstance().getHwid();
 	}
 
 	@Kroll.method
-	public String getPushToken()
-	{
+	public String getPushToken() {
 		return Pushwoosh.getInstance().getPushToken();
 	}
 
 	@Kroll.method
-	public Map getNotificationSettings()
-	{
+	public Map getNotificationSettings() {
 		boolean enabled = PushwooshNotificationSettings.areNotificationsEnabled();
 
 		return Collections.singletonMap("enabled", enabled);
@@ -305,8 +281,7 @@ public class PushnotificationsModule extends KrollModule
 		}
 	}
 
-	private void onNotificationOpened(final String messageData)
-	{
+	private void onNotificationOpened(final String messageData) {
 		if (!initialized.get() || !isTitaniumReady()) {
 			saveStartPush(messageData);
 			return;
@@ -314,8 +289,7 @@ public class PushnotificationsModule extends KrollModule
 
 		mainHandler.post(new Runnable() {
 			@Override
-			public void run()
-			{
+			public void run() {
 				notifyPushOpened(messageData);
 			}
 		});
@@ -333,12 +307,10 @@ public class PushnotificationsModule extends KrollModule
 		}
 	}
 
-	private void onRegistrationSucceed(final String registrationId)
-	{
+	private void onRegistrationSucceed(final String registrationId) {
 		mainHandler.post(new Runnable() {
 			@Override
-			public void run()
-			{
+			public void run() {
 				HashMap data = new HashMap();
 				data.put("registrationId", registrationId);
 
@@ -353,8 +325,7 @@ public class PushnotificationsModule extends KrollModule
 	private void onRegistrationFailed(final String error) {
 		TiApplication.getInstance().getRootActivity().runOnUiThread(new Runnable() {
 			@Override
-			public void run()
-			{
+			public void run() {
 				HashMap data = new HashMap();
 				data.put("error", error);
 
@@ -366,8 +337,7 @@ public class PushnotificationsModule extends KrollModule
 		});
 	}
 
-	private HashMap<String, Object> convertMessageData(String messageData)
-	{
+	private HashMap<String, Object> convertMessageData(String messageData) {
 		HashMap<String, Object> result = new HashMap<String, Object>();
 		try {
 			JSONObject json = new JSONObject(messageData);
@@ -382,15 +352,14 @@ public class PushnotificationsModule extends KrollModule
 			if (userData != null) {
 				result.put("extras", JsonUtils.jsonToMap(userData));
 			}
-		} catch(Exception e) {
+		} catch (Exception e) {
 			Log.e(LCAT, "Failed to convert push message data", e);
 		}
 
 		return result;
 	}
 
-	private boolean isTitaniumReady()
-	{
+	private boolean isTitaniumReady() {
 		if (TiApplication.getInstance() == null) {
 			return false;
 		}
@@ -403,7 +372,7 @@ public class PushnotificationsModule extends KrollModule
 	}
 
 
-	static boolean onPushOpened(String message){
+	static boolean onPushOpened(String message) {
 		if (INSTANCE != null) {
 			INSTANCE.onNotificationOpened(message);
 			return true;
@@ -414,9 +383,8 @@ public class PushnotificationsModule extends KrollModule
 		}
 	}
 
-	static boolean onPushReceived(final String messageData, boolean foreground)
-	{
-		if(INSTANCE == null){
+	static boolean onPushReceived(final String messageData, boolean foreground) {
+		if (INSTANCE == null) {
 			return false;
 		}
 
@@ -427,11 +395,10 @@ public class PushnotificationsModule extends KrollModule
 
 		TiApplication.getInstance().getRootActivity().runOnUiThread(new Runnable() {
 			@Override
-			public void run()
-			{
+			public void run() {
 				INSTANCE.notifyPushReceived(messageData);
 
-				if(foreground && INSTANCE.broadcastPush) {
+				if (foreground && INSTANCE.broadcastPush) {
 					INSTANCE.notifyPushOpened(messageData);
 				}
 			}
@@ -440,8 +407,7 @@ public class PushnotificationsModule extends KrollModule
 		return foreground && INSTANCE.broadcastPush;
 	}
 
-	private static void saveStartPush(String messageData)
-	{
+	private static void saveStartPush(String messageData) {
 		Log.i(LCAT, "Start push saved: " + messageData);
 		startPushData.set(messageData);
 	}
