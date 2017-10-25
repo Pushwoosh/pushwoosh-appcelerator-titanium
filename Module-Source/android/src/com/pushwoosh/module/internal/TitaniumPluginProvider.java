65553d629da98ddbb9ff4d59fc3d730ad18f2489
Mikhail Savin <smu@arello-mobile.com>
2017-10-25 17:48:11 +0700

    [feature][Android] Up pushwoosh Android Sdk library to 5.4.1

--- Module-Source/android/src/com/pushwoosh/module/internal/TitaniumPluginProvider.java
+++ Module-Source/android/src/com/pushwoosh/module/internal/TitaniumPluginProvider.java
@@ -7,4 +7,9 @@ public class TitaniumPluginProvider implements PluginProvider {
 	public String getPluginType() {
 		return "Titanium";
 	}
+
+	@Override
+	public int richMediaStartDelay() {
+		return DEFAULT_RICH_MEDIA_START_DELAY;
+	}
 }
