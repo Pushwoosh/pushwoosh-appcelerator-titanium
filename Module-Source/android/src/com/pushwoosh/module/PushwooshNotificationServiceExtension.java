626bd819a6c4a08565ce60c7a1a5574b39fdb276
Mikhail Savin <smu@arello-mobile.com>
2017-10-30 12:46:27 +0700

    [fixbug][Android] Fix bug with incorrect activity opened when click on push

--- Module-Source/android/src/com/pushwoosh/module/PushwooshNotificationServiceExtension.java
+++ Module-Source/android/src/com/pushwoosh/module/PushwooshNotificationServiceExtension.java
@@ -1,5 +1,22 @@
 package com.pushwoosh.module;
 
+import org.appcelerator.kroll.KrollDict;
+import org.appcelerator.kroll.KrollEventCallback;
+import org.appcelerator.kroll.KrollFunction;
+import org.appcelerator.kroll.KrollModule;
+import org.appcelerator.kroll.annotations.Kroll;
+
+import org.appcelerator.titanium.*;
+import org.appcelerator.titanium.TiC;
+import org.appcelerator.titanium.proxy.IntentProxy;
+import org.appcelerator.kroll.common.Log;
+import org.appcelerator.kroll.common.TiConfig;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.pushwoosh.Pushwoosh;
 import com.pushwoosh.notification.NotificationServiceExtension;
 import com.pushwoosh.notification.PushMessage;
 
@@ -12,8 +29,36 @@ public class PushwooshNotificationServiceExtension extends NotificationServiceEx
 
 	@Override
 	protected void startActivityForPushMessage(final PushMessage pushMessage) {
-		super.startActivityForPushMessage(pushMessage);
-		PushnotificationsModule.onPushOpened(pushMessage.toJson().toString());
+		Bundle extras = new Bundle();
+		extras.putString(Pushwoosh.PUSH_RECEIVE_EVENT, pushMessage.toJson().toString());
+		TiApplication appContext = TiApplication.getInstance();
+		Activity activity = appContext.getCurrentActivity();
+		if (activity == null) {
+			activity = appContext.getRootActivity();
+		}
 
+		Intent launchIntent = null;
+		if(activity != null) {
+			launchIntent = activity.getIntent();
+		} else {
+			launchIntent = getApplicationContext().getPackageManager().getLaunchIntentForPackage(getApplicationContext().getPackageName());
+			if(launchIntent == null){
+				super.startActivityForPushMessage(pushMessage);
+				return;
+			}
+
+			launchIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+		}
+
+		launchIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+		launchIntent.putExtras(extras);
+
+		getApplicationContext().startActivity(launchIntent);
+	}
+
+	@Override
+	protected void onMessageOpened(PushMessage pushMessage) {
+		super.onMessageOpened(pushMessage);
+		PushnotificationsModule.onPushOpened(pushMessage.toJson().toString());
 	}
 }
