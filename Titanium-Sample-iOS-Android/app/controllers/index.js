2ef744dc70a7b9fe90dd3e36bbe70c11a6f6ef7a
Vitaly Romanychev <vromanychev@pushwoosh.com>
2021-09-16 16:05:42 +0700

    [feature][PUSH-29408] update native SDKs in titanium modules

--- Titanium-Sample-iOS-Android/app/controllers/index.js
+++ Titanium-Sample-iOS-Android/app/controllers/index.js
@@ -36,6 +36,16 @@ Ti.API.info('Pushwoosh hwid: ' + pushwoosh.getHwid());
 
 Ti.API.info("Notification settings: " + JSON.stringify(pushwoosh.getNotificationSettings()));
 
+//start and stop location tracking
+var pushwooshGeozones = require('com.pushwoosh.geozones');
+function doClickEnableGeozones(e){
+  pushwooshGeozones.startTrackingGeoPushes();
+};
+function doClickDisableGeozones(e){
+  pushwooshGeozones.stopTrackingGeoPushes();
+};
+
+
 // Application icon badges
 //pushwoosh.setBadgeNumber(5);
 //pushwoosh.addBadgeNumber(3);
@@ -44,11 +54,6 @@ Ti.API.info("Notification settings: " + JSON.stringify(pushwoosh.getNotification
 // Segmentation
 //pushwoosh.setTags({deviceName:"hello", deviceId:10});
 //pushwoosh.setTags({"MyTag":["hello", "world"]});
-
-// Geopushes
-// var pushwooshGeozones = require('com.pushwoosh.geozones');
-// pushwooshGeozones.startTrackingGeoPushes();
-// pushwooshGeozones.stopTrackingGeoPushes();
 
 // Inapp & Events
 //pushwoosh.setUserId("pushwooshid%42");
